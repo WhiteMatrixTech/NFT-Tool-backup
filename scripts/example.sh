60cc2c9816cc0801847cceb9b39165869c44c4ce
kilru <tim.whitematrix@gmail.com>
2021-06-22 14:15:33 +0800

    ignore the data folder and refactoring

--- scripts/example.sh
+++ scripts/example.sh
@@ -2,5 +2,7 @@
 
 cd ..
 docker build . -t renderjob
+ROOT=$(pwd)
+echo "Path is $ROOT/data"
 cd scripts
-docker run -it renderjob bash "./scripts/test_run_pawn.sh"
+docker run -v $ROOT/data:/data -it renderjob bash "./scripts/test_run_pawn.sh"
